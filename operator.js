ac2f86a21e921f35628d7ea1bd2a134a3270a55c
abdul <abdulhafidz3105@gmail.com>
2024-11-16 06:37:25 +0700

    operator perbandingan, operator logika

--- operator.js
+++ operator.js
@@ -57,5 +57,65 @@ console.log(`(12 + 5) * 8 = ${tandaKurung}`);
 console.log("\n");
 
 /*
-
+Comparison Operator(perbandingan)
+Operator perbandingan adalah operator yang digunakan untuk membandingkan dua nilai dan mengembalikan nilai boolean (true atau false) sebagai hasil perbandingan. Salah satu penggunaan operator perbandingan adalah ketika kita berhadapan dengan skenario “Apakah x lebih besar dari y?” dan mendapatkan hasil dalam bentuk “Ya” atau “Tidak”.
 */
+console.log("COMPARISON OPERATOR");
+const perbandingan1 = 12;
+const perbandingan2 = '20';
+/*
+Membandingkan dua operan, tanpa memperhatikan tipe data. Jika nilai kedua operan dianggap sama, operasi akan mengembalikan true, sebaliknya false.
+*/
+console.log(perbandingan1 == perbandingan2); // 12 == '20' = False
+/*
+Membandingkan dua operan, tanpa memperhatikan tipe data. Jika nilai kedua operan dianggap tidak sama, operasi akan mengembalikan true, sebaliknya false. 
+*/
+console.log(perbandingan1 != perbandingan2); // 12 != '20' = True
+/*
+Membandingkan dua operan beserta tipe datanya. Jika kedua operan identik, operasi akan mengembalikan true, sebaliknya false.
+*/
+console.log(perbandingan1 === perbandingan2); // 12 === '20' = False
+/*
+Membandingkan dua operan beserta tipe datanya. Jika kedua operan tidak identik, operasi akan mengembalikan true, sebaliknya false.
+*/
+console.log(perbandingan1 !== perbandingan2); // 12 !== '20' = True
+/*
+Membandingkan operan pertama apakah lebih besar dari operan kedua. Jika iya mengembalikkan pernyataan true, jika salah mengembalikkan pernyataan false.
+*/
+console.log(perbandingan1 > perbandingan2); // 12 > '20' == False
+/*
+Membandingkan operan pertama apakan lebih besar sama dengan dari operan kedua. Jika iya mengembalikkan pernyataan true, jika salah mengembalikkan pernyataan false.
+*/
+console.log(perbandingan1 >= perbandingan2); // 12 >= '20' = False
+/*
+Membandingkan operan pertama apakah lebih kecil dari operan kedua. Jika iya mengembalikkan pernyataan true, jika salah mengembalikkan pernyataan false.
+*/
+console.log(perbandingan1 < perbandingan2); // 12 < '20' = True
+/*
+Membandingkan operan pertama apakan lebih kecil1 sama dengan dari operan kedua. Jika iya mengembalikkan pernyataan true, jika salah mengembalikkan pernyataan false.
+*/
+console.log(perbandingan1 <= perbandingan2); // 12 <= '20' = True
+
+console.log("\n");
+
+/*
+Logical Operator
+Operator logika digunakan untuk menetapkan logika dari dua nilai operan boolean. Dengan operator logika, kita bisa melakukan operasi gerbang logika, seperti AND dan OR. Operator logika dapat digunakan untuk menetapkan logika yang lebih kompleks.
+*/
+console.log("LOGICAL OPERATOR");
+const perbandingan3 = true;
+const perbandingan4 = false;
+
+// AND
+console.log("AND(&&)");
+console.log("True  &&  False = " + (perbandingan3 && perbandingan4));
+console.log("True  &&  True = " + (perbandingan3 && perbandingan3));
+console.log("False &&  True = " + (perbandingan4 && perbandingan3));
+console.log("False &&  False = " + (perbandingan4 && perbandingan4));
+
+// OR
+console.log("OR(||)");
+console.log("True  ||  False = " + (perbandingan3 || perbandingan4));
+console.log("True  ||  True = " + (perbandingan3 || perbandingan3));
+console.log("False ||  True = " + (perbandingan4 || perbandingan3));
+console.log("False ||  False = " + (perbandingan4 || perbandingan4));
